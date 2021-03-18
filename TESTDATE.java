94f8f3c5b72c6be079cf7d5432cd7644b3b5a185
chaleomrat <614259007@webmail.npru.ac.th>
2021-03-18 13:39:56 +0700

    ses

--- TESTDATE.java
+++ TESTDATE.java
@@ -1,106 +1,81 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-class showDate {
-
+class TESTDATE {
   @Test
-  void TC1001() throws InterruptedException {
+  void testdata1() {
     WebDriver driver = null;
     String browser = "firefox";
-
     if (browser.equalsIgnoreCase("firefox")) {
+
       System.setProperty("webdriver.gecko.driver", "C:\\Users\\sometimes\\Desktop\\geckodriver.exe");
       driver = new FirefoxDriver();
     }
     driver.get("http://localhost:8080/Date/");
-    driver.manage().window().setSize(new Dimension(958, 900));
     driver.findElement(By.id("month")).click();
     {
       WebElement dropdown = driver.findElement(By.id("month"));
-      dropdown.findElement(By.xpath("//option[. = 'February']")).click();
+      dropdown.findElement(By.xpath("//option[. = 'March']")).click();
     }
-    driver.findElement(By.id("month")).click();
     driver.findElement(By.id("day")).click();
     driver.findElement(By.id("day")).sendKeys("02");
     driver.findElement(By.id("submit")).click();
-    driver.findElement(By.id("month")).click();
-    {
-      WebElement dropdown = driver.findElement(By.id("month"));
-      dropdown.findElement(By.xpath("//option[. = 'March']")).click();
-    }
-    driver.findElement(By.id("month")).click();
-    driver.findElement(By.id("submit")).click();
-
+    String result = driver.findElement(By.id("result")).getText();
+    assertEquals("Tuesday", result);
     driver.close();
   }
 
   @Test
-  void TC1002() throws InterruptedException {
+  // pass
+  void testdata2() {
     WebDriver driver = null;
     String browser = "firefox";
-
     if (browser.equalsIgnoreCase("firefox")) {
+
       System.setProperty("webdriver.gecko.driver", "C:\\Users\\sometimes\\Desktop\\geckodriver.exe");
       driver = new FirefoxDriver();
     }
     driver.get("http://localhost:8080/Date/");
-    driver.manage().window().setSize(new Dimension(958, 900));
     driver.findElement(By.id("month")).click();
     {
       WebElement dropdown = driver.findElement(By.id("month"));
-      dropdown.findElement(By.xpath("//option[. = 'January']")).click();
+      dropdown.findElement(By.xpath("//option[. = 'June']")).click();
     }
-    driver.findElement(By.id("month")).click();
     driver.findElement(By.id("day")).click();
     driver.findElement(By.id("day")).sendKeys("15");
     driver.findElement(By.id("submit")).click();
-    driver.findElement(By.id("month")).click();
-    {
-      WebElement dropdown = driver.findElement(By.id("month"));
-      dropdown.findElement(By.xpath("//option[. = 'March']")).click();
-    }
-    driver.findElement(By.id("month")).click();
-    driver.findElement(By.id("submit")).click();
-
+    String result = driver.findElement(By.id("result")).getText();
+    assertEquals("Tuesday", result);
     driver.close();
   }
 
   @Test
-  void TC1003() throws InterruptedException {
+  // pass
+  void testdata3() {
     WebDriver driver = null;
     String browser = "firefox";
-
     if (browser.equalsIgnoreCase("firefox")) {
+
       System.setProperty("webdriver.gecko.driver", "C:\\Users\\sometimes\\Desktop\\geckodriver.exe");
       driver = new FirefoxDriver();
     }
     driver.get("http://localhost:8080/Date/");
-    driver.manage().window().setSize(new Dimension(958, 900));
     driver.findElement(By.id("month")).click();
     {
       WebElement dropdown = driver.findElement(By.id("month"));
-      dropdown.findElement(By.xpath("//option[. = 'April']")).click();
+      dropdown.findElement(By.xpath("//option[. = 'June']")).click();
     }
-    driver.findElement(By.id("month")).click();
     driver.findElement(By.id("day")).click();
-    driver.findElement(By.id("day")).sendKeys("20");
+    driver.findElement(By.id("day")).sendKeys("13");
     driver.findElement(By.id("submit")).click();
-    driver.findElement(By.id("month")).click();
-    {
-      WebElement dropdown = driver.findElement(By.id("month"));
-      dropdown.findElement(By.xpath("//option[. = 'March']")).click();
-    }
-    driver.findElement(By.id("month")).click();
-    driver.findElement(By.id("submit")).click();
-
+    String result = driver.findElement(By.id("result")).getText();
+    assertEquals("Sunday", result);
     driver.close();
-  };
+  }
 
 };
